fc66bc0f242e4585afba350e9663a2c14583ac00
006lp <144674902+006lp@users.noreply.github.com>
2024-04-05 00:59:04 +0800

    重启数据库驱动，引擎，编码的环境变量

--- config.inc.php
+++ config.inc.php
@@ -7,19 +7,19 @@
  * @version    $Id$
  */
 /** 开启https */ 
-define('__TYPECHO_SECURE__',true);
+define('__TYPECHO_SECURE__'，true);
 
 /** 定义根目录 */
-define('__TYPECHO_ROOT_DIR__', dirname(__FILE__));
+define('__TYPECHO_ROOT_DIR__'， dirname(__FILE__));
 
 /** 定义插件目录(相对路径) */
-define('__TYPECHO_PLUGIN_DIR__', '/usr/plugins');
+define('__TYPECHO_PLUGIN_DIR__'， '/usr/plugins');
 
 /** 定义模板目录(相对路径) */
-define('__TYPECHO_THEME_DIR__', '/usr/themes');
+define('__TYPECHO_THEME_DIR__'， '/usr/themes');
 
 /** 后台路径(相对路径) */
-define('__TYPECHO_ADMIN_DIR__', '/admin/');
+define('__TYPECHO_ADMIN_DIR__'， '/admin/');
 
 /** 设置包含路径 */
 @set_include_path(get_include_path() . PATH_SEPARATOR .
@@ -33,15 +33,15 @@ require_once 'Typecho/Common.php';
 Typecho_Common::init();
 
 /** 定义数据库参数(通过环境变量) */
-$db = new Typecho_Db('Pdo_Mysql', 'typecho_');
+$db = new Typecho_Db($_ENV["ADAPTER"], 'typecho_');
 $db->addServer(array (
   'host' => $_ENV["HOST"],
   'user' => $_ENV["USERNAME"],
   'password' => $_ENV["PASSWORD"],
-  'charset' => 'utf8mb4',
+  'charset' => $_ENV["CHARSET"]，
   'port' => $_ENV["PORT"],
   'database' => $_ENV["DATABASE"],
-  'engine' => 'InnoDB',
+  'engine' => $_ENV["ENGINE"]，
 ), Typecho_Db::READ | Typecho_Db::WRITE);
 Typecho_Db::set($db);
 
